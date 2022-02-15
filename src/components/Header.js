d264a54dce59ba8091d8d363e28310ab66b79439
Anna Tuban <mshannahstarling@gmail.com>
2022-02-15 21:12:35 +0300

    feat: add header styles and logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import Logo from './Logo';
 
 const Header = () => {
   return (
     <header className='header'>
+      <Logo />
       <h1 className='content__title'>Meal&nbsp;planning</h1>
     </header>
   );
---
+++ src/components/Logo.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Logo() {
+  return (
+    <img
+      className='logo__pic'
+      alt='logo'
+      src='https://freesvg.org/img/Gerald-G-Fast-Food-Lunch-Dinner-FF-Menu-28.png'
+    />
+  );
+}
+
+export default Logo;
