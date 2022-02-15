09be118daa5317987f4e0c45f31d39b5a41713ab
Anna Tuban <mshannahstarling@gmail.com>
2022-02-15 20:56:07 +0300

    feat: add Header component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Header from './components/Header';
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Header />
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header className='header'>
+      <h1 className='content__title'>Meal&nbsp;planning</h1>
+    </header>
+  );
+};
+
+export default Header;
