4c690cfb0e442b9f3e0d3a13884a28e8b04b90bc
Anna Tuban <mshannahstarling@gmail.com>
2022-02-15 21:20:34 +0300

    feat: add menu nav

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Logo from './Logo';
+import Menu from './UI/nav/Menu';
 
 const Header = () => {
   return (
     <header className='header'>
       <Logo />
       <h1 className='content__title'>Meal&nbsp;planning</h1>
+      <Menu className='menu__item' />
     </header>
   );
 };
---
+++ src/components/UI/nav/Menu.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+function Menu({ className }) {
+  return (
+    <nav className='menu'>
+      <NavLink exact to='/' className={className} activeClassName={`${className}_current`}>
+        about
+      </NavLink>
+      <NavLink to='/recipes' className={className} activeClassName={`${className}_current`}>
+        recipes
+      </NavLink>
+      <NavLink to='/shoppinglist' className={className} activeClassName={`${className}_current`}>
+        shopping&nbsp;list
+      </NavLink>
+      <NavLink to='/calendar' className={className} activeClassName={`${className}_current`}>
+        calendar
+      </NavLink>
+      <NavLink to='/ newrecipe' className={className} activeClassName={`${className}_current`}>
+        new&nbsp;recipe
+      </NavLink>
+    </nav>
+  );
+}
+
+export default Menu;
